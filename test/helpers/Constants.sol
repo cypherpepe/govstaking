d6295644734a8d7ac01f4c9cb7d2c7eb147de512
Ben DiFrancesco <ben@scopelift.co>
2024-09-10 16:38:07 -0400

    WIP: remove forge-std

--- test/helpers/Constants.sol
+++ test/helpers/Constants.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.23;
 
-import {ISwapRouter} from "v3-periphery/interfaces/ISwapRouter.sol";
-
 contract Constants {
   address constant UNISWAP_GOVERNOR_ADDRESS = 0x408ED6354d4973f66138C91495F2f2FCbd8724C3;
   address constant WBTC_WETH_3000_POOL = 0xCBCdF9626bC03E24f779434178A73a0B4bad62eD;
@@ -12,7 +10,5 @@ contract Constants {
   address constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // use deposit
   address constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // mint with auth
   address constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //  mint only minters
-  ISwapRouter constant UNISWAP_V3_SWAP_ROUTER =
-    ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
   address constant STAKING_TOKEN_MINTER = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
 }
