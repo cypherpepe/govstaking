4d02795ffc2a94f6a2ed52dcfb1ce9bbfe3856e3
Ben DiFrancesco <ben@scopelift.co>
2024-09-10 16:29:40 -0400

    WIP: remove lots of v3factoryowner related stuff

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,10 +8,8 @@ import {Script} from "forge-std/Script.sol";
 
 import {DeployInput} from "script/DeployInput.sol";
 import {UniStaker} from "src/UniStaker.sol";
-import {V3FactoryOwner} from "src/V3FactoryOwner.sol";
 import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
 import {INotifiableRewardReceiver} from "src/interfaces/INotifiableRewardReceiver.sol";
-import {IUniswapV3FactoryOwnerActions} from "src/interfaces/IUniswapV3FactoryOwnerActions.sol";
 
 contract Deploy is Script, DeployInput {
   uint256 deployerPrivateKey;
@@ -23,7 +21,7 @@ contract Deploy is Script, DeployInput {
     );
   }
 
-  function run() public returns (V3FactoryOwner, UniStaker) {
+  function run() public returns (UniStaker) {
     vm.startBroadcast(deployerPrivateKey);
     // Deploy the staking contract
     UniStaker uniStaker = new UniStaker(
@@ -32,22 +30,10 @@ contract Deploy is Script, DeployInput {
       vm.addr(deployerPrivateKey)
     );
 
-    // Deploy a new owner for the V3 factory owner actions contract.
-    V3FactoryOwner v3FactoryOwner = new V3FactoryOwner(
-      UNISWAP_GOVERNOR_TIMELOCK,
-      IUniswapV3FactoryOwnerActions(UNISWAP_V3_FACTORY_ADDRESS),
-      IERC20(PAYOUT_TOKEN_ADDRESS),
-      PAYOUT_AMOUNT,
-      INotifiableRewardReceiver(uniStaker)
-    );
-
-    // Enable the v3FactoryOwner as a UniStaker reward notifier
-    uniStaker.setRewardNotifier(address(v3FactoryOwner), true);
-
     // Change UniStaker admin from `msg.sender` to the Governor timelock
     uniStaker.setAdmin(UNISWAP_GOVERNOR_TIMELOCK);
     vm.stopBroadcast();
 
-    return (v3FactoryOwner, uniStaker);
+    return uniStaker;
   }
 }
